46218b2594e7247bd9d0aff72fe69cde3496a4ef
Lawani-EJ <elyonjohn60@gmail.com>
2024-06-19 09:11:46 +0100

    Implementing state management and form hanling in react component In this commit i refactored the exesting  component to explore and implement various examples of state management using the  hook in react. Each example demonstrates different aspects of handling state and rendering components conditionally based on state changes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // Example1
+  // const show = useState('Geeks For Geeks');
+  // return (
+  //   <h1>Welcome to{show}</h1>
+  // );
+
+  // Example2
+  // const [Click, setClick] = useState(0);
+
+  // Exapmle4
+  // const [click, setClick] = useState([]);
+  // const addNumber = () => {
+  //   setClick([
+  //     ...click,{
+  //       id: click.length,
+  //       Value: Math.random()*10
+  //     }
+  //   ]);
+  // };
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return(
+    // Example3
+    // <div>
+    //   <p>You Clciked {Click} times</p>
+    //   <button onClick={() => setClick(Click + 1)}>Click Me</button>
+    // </div>
+
+    // <div>
+    //   <p>You Clicked {Click} times</p>
+    //   <p>The number of times you have clicked {Click %2 === 0 ? 'even!' : 'odd!'}</p>
+    //   <button onClick={() =>setClick(Click => Click + 1)}>Click Me!!!</button>
+    // </div>
+
+    // Example4
+    // <div>
+    //   <ul>
+    //     {click.map (item =>(<li key={item.id}>{item.Value}</li>))}
+    //   </ul>
+    //   <button onClick={addNumber}>Clcik ME!!</button>
+    // </div>
+
+    <form>
+      <h1>The Username typed in is : {username}</h1>
+      <input type='text' value={username} onChange={(e) => {setUsername(e.target.value)}}></input>
+      <h1>The password typed in is: {password}</h1>
+      <input type='password' value={password} onChange={(e) => {setPassword(e.target.value)}}></input>
+    </form>
+
   );
 }
 
